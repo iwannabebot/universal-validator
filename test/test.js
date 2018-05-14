b7ffea4d7c5185d357283b56fd1e0103f8cf9d39
iwannabebot <nilayvishwakarma@live.com>
2018-05-15 01:06:06 +0530

    basic test

---
+++ test/test.js
@@ -0,0 +1,7 @@
+'use strict';
+var expect = require('chai').expect;
+describe('simple validate test', () => {
+    it('should return true', () => {
+        expect(true).to.equal(true);
+    });
+});
